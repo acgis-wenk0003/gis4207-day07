37e6397a1b83e801914c5cbe1f8db975ae87cf8a
LAPTOP-KV4DTCCF\chris <wenk0003@algonquinlive.com>
2018-02-27 15:09:55 -0500

    no message

--- ChriswandMorganH/src/VarsAndExp/Program.cs
+++ ChriswandMorganH/src/VarsAndExp/Program.cs
@@ -13,11 +13,21 @@ namespace VarsAndExp
         }
     
     
-        static double Dms2Dd(int deg, int min, double sec, string bearing)
+    ///  convert degree minutes seconds to decimal degrees
+        public static double Dms2Dd(int deg, int min, double sec, string bearing)
         {
             int minutes = min / 60;
             double seconds = sec / 3600; 
             return deg + minutes + seconds;
         }
     }
+    class Test_Program
+    {
+        void test()
+        {
+           
+           double p = Program.Dms2Dd(50, 40, 20.11, "N");
+           
+        }
+    }    
 }
